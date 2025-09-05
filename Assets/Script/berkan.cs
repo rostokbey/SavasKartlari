50468e5e710fc5eb1122daba63e7b3879329dc03
rostokbey <fahri0695@gmail.com>
2025-09-06 00:04:28 +0300

    Berkan deneme 06092025
    Ufak değişiklik denemesi

---
+++ Assets/Script/berkan.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class berkan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//  berkan naber lan
